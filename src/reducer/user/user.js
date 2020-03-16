ba3c0b9bee0f2b4cfcece5ee18e20c4e56b126ff
gulnaraY <guga.ufa@gmail.com>
2020-03-16 18:17:14 +0300

    добавляет user login

--- src/reducer/user/user.js
+++ src/reducer/user/user.js
@@ -41,6 +41,16 @@ const Operation = {
       throw err;
     });
   },
+
+  login: (authData) => (dispatch, getState, api) => {
+    return api.post(`/login`, {
+      email: authData.login,
+      password: authData.password,
+    })
+    .then(() => {
+      dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH));
+    });
+  }
 };
 
 export {
